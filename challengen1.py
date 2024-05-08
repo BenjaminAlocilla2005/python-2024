c0777e8f3d37800468183d5d07bf77d8a79c72e7
BenjaminAlocilla2005 <168034814+BenjaminAlocilla2005@users.noreply.github.com>
2024-05-08 16:48:55 -0400

    Add files via upload

---
+++ challengen1.py
@@ -0,0 +1,13 @@
+# Este es un ChallengeN1
+print ("ChallengeN1")
+
+# 1. Declaracion de Variable
+nombre = "Benjamin Nicolas"
+edad = 18
+edad_siguiente = 20
+
+total_edad = edad + edad_siguiente
+
+#Print Clasico
+print (f"hola mi nombre es {nombre}, tengo {edad} años y en 20 años tendre {total_edad} años")
+
