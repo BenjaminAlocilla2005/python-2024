f0a3b9ddcbb5c3ac374aec6ebf4a821857f0da17
BenjaminAlocilla2005 <168034814+BenjaminAlocilla2005@users.noreply.github.com>
2024-05-22 16:55:50 -0400

    Add files via upload

---
+++ algoritmo1.py
@@ -0,0 +1,26 @@
+#este es un algoritmo1
+print("algoritmo1")
+
+#Declaracion de Variable
+notaj1= 5.64
+notaj2= 3.45
+notaj3= 4.56
+
+notaa1= 4.56
+notaa2= 5.64
+notaa3= 6.91
+
+notap1= 3.45
+notap2= 4.01
+notap3= 6.23
+
+promediojoel= round((notaj1 + notaj2 + notaj3)/3,3)
+promedioalondra= round((notaa1 + notaa2 + notaa3)/3,3)
+promediopaz= round((notap1 + notap2 + notap3)/3,3)
+
+notadefinitivaJ= round(promediojoel,3)
+notadefinitivaA= round(promedioalondra,3)
+notadefinitivaP= round(promediopaz,3)
+
+#Print Clasico
+print (f"la nota definitiva de Joel es {notadefinitivaJ}, la de Alondra es {notadefinitivaA} y la de Paz es {notadefinitivaP}")
